f0996be2dbb93e9ae753e496c96e18f2ec757dd1
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-04-21 17:31:46 -0700

    updating search query, new styles

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,27 @@
 class Item < ApplicationRecord
+  attr_accessor :locations
+
+  def locations
+    @locations || [
+      'No location submitted',
+      'Doe Circle',
+      'Doe North Entrance',
+      'Doe South Entrance',
+      'Gardner Stacks',
+      'Library Security',
+      'Moffit Third Floor',
+      'Moffit forth Floor',
+      'Moffit Circle',
+      'MRC',
+      'Privileges Desk',
+      'UCPD'
+    ]
+
+  end
     scope :claimed, -> { where("itemStatus = 3")}
     scope :found, -> { where("itemStatus = 1")}
-    scope :query_params, lambda {|params| where("itemDescription LIKE ?", "%#{params[:keyword]}%") } 
+    scope :query_params, lambda {
+      |params| where(
+      "itemDescription LIKE ?  AND itemLocation = ?", "%#{params[:keyword]}%", "#{params[:itemLocation]}")
+    }
   end
