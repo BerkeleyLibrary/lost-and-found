f556e3149f7ca8f14e87d4f7899c1958cd93c219
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-04-22 17:09:37 -0700

    updating search query to handle all libraries

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,12 @@ class Item < ApplicationRecord
 <<<<<<< HEAD
   attr_accessor :locations
   attr_accessor :types
+<<<<<<< HEAD
 =======
   attr_accessor :locations
 >>>>>>> updating search query, new styles
+=======
+>>>>>>> updating search query to handle all libraries
 
   def locations
     @locations || [
@@ -22,6 +25,7 @@ class Item < ApplicationRecord
       'Privileges Desk',
       'UCPD'
     ]
+<<<<<<< HEAD
 <<<<<<< HEAD
   end
 
@@ -50,14 +54,40 @@ class Item < ApplicationRecord
 end
 =======
 =======
+=======
+  end
+>>>>>>> updating search query to handle all libraries
 
+  def types
+    @types || [
+      'none',
+      'book',
+      'clothing',
+      'electronics',
+      'glasses',
+      'id',
+      'keys',
+      'mp3',
+      'other',
+    ]
   end
 >>>>>>> updating search query, new styles
     scope :claimed, -> { where("itemStatus = 3")}
     scope :found, -> { where("itemStatus = 1")}
+<<<<<<< HEAD
     scope :query_params, lambda {
       |params| where(
       "itemDescription LIKE ?  AND itemLocation = ?", "%#{params[:keyword]}%", "#{params[:itemLocation]}")
     }
   end
 >>>>>>> Adding basic CRUD features to insert and search on keywords
+=======
+    scope :query_params, -> (params ) {
+      unless params[:searchAll]
+       where("itemType = ? AND itemDescription LIKE ? AND itemLocation = ?", "#{params[:itemType]}","%#{params[:keyword]}%", "#{params[:itemLocation]}")
+      else
+        where("itemType = ? AND itemDescription LIKE ?  ", "#{params[:itemType]}", "%#{params[:keyword]}%")
+      end
+      }
+end
+>>>>>>> updating search query to handle all libraries
