170770adeb24533726fd8460bb75edf285af4201
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-06-30 17:09:49 -0700

    removing unfinished test

--- spec/features/claimed_items_spec.rb
+++ spec/features/claimed_items_spec.rb
@@ -8,27 +8,4 @@ RSpec.describe 'Search form tasks', type: :feature do
         Capybara.current_session.driver.browser.set_cookie "user_name=Dante"
     end
 
-    scenario 'Setting item to claimed reveals Found By field' do
-        visit '/insert_form'
-        fill_in 'itemDescription', with: "TEST_ITEM"
-        find('input[name="commit"]').click
-        click_link "Search"
-        click_link "Show all found items"
-        first('td').click_link('Edit')
-        within '#itemStatus' do
-            find("option[value='1\']").click
-        end
-
-        p '============='
-        p page.body
-        p '============='
-        find('#claimedByLabel').should_not be_visible
-        within '#itemStatus' do
-            find("option[value='3\]").click
-          end
-          p '==========='
-          p page.body
-          p '==========='
-        expect(page).to have_selector('#claimedByLabel', visible: true) 
-    end
 end
