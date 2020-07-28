55d4fc89b5b64f195b1d5a5a5ac2a934924cd748
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-07-28 16:22:23 -0700

    pagination v1 - re-order

--- app/assets/javascripts/lostandfound.js
+++ app/assets/javascripts/lostandfound.js
@@ -1,4 +1,15 @@
 
+ function pagination_setup() {
+  $(document).ready( function () {
+    console.log( "ready!" );
+    $('#found_items_table').DataTable({
+        "dom": '<"top"ip>rt<"bottom"><"clear">'
+    } );
+    $('#claimed_items_table').DataTable({
+        "dom": '<"top"ip>rt<"bottom"><"clear">'
+    } );
+    } );
+  }
 $(document).on("change", "#itemStatus", function(e){
     var select_val = $(this).val();
     if (select_val == 3) {
