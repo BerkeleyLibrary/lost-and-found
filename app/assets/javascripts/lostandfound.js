09baa9fecf9957a91296f9c4b8d3e0c6e54954d1
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-07-29 09:30:49 -0700

    loading spinner

--- app/assets/javascripts/lostandfound.js
+++ app/assets/javascripts/lostandfound.js
@@ -3,9 +3,15 @@
     $('#found_items_table').DataTable({
         "dom": '<"top"ip>rt<"bottom"><"clear">'
     } );
+
+    $('#found_items_spinner').hide();
+    $('#found_items_table').show();
+
     $('#claimed_items_table').DataTable({
         "dom": '<"top"ip>rt<"bottom"><"clear">'
     } );
+    $('#claimed_items_spinner').hide();
+    $('#claimed_items_table').show();
   }
 $(document).on("change", "#itemStatus", function(e){
     var select_val = $(this).val();
