0a4315af824c39264bfcfbecb3ad202d67086aae
David Moles <dmoles@berkeley.edu>
2021-10-28 16:14:02 -0700

    Remove old specs

--- spec_old/edit_history_spec.rb
+++
@@ -1,9 +0,0 @@
-describe 'RSpec test group' do
-  it 'by default, PaperTrail will be turned off' do
-    expect(PaperTrail).to_not be_enabled
-  end
-
-  it 'can be turned on at the `it` or `describe` level', versioning: true do
-    expect(PaperTrail).to be_enabled
-  end
-end
