54b282a0260cc16ae3c8fb4d6e03d52413c11934
David Zuckerman <dzuckerm@berkeley.edu>
2026-07-30 08:07:08 -0700

    Updating omniauth, adding login page, updating docker compose for development instead of prod environment (#26)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # that allows you to enter the listed User attributes.
   unless Rails.env.production?
     provider :developer,
-             fields: %i[uid user_name],
+             fields: %i[uid display_name employee_id],
              uid_field: :uid
   end
 
@@ -12,11 +12,21 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     "auth#{'-test' unless Rails.env.production?}.berkeley.edu"
   end
 
+  fetch_raw_info = proc do |_strategy, _opts, _ticket, _user_info, rawxml|
+    next {} if rawxml.empty?
+
+    groups_txt = rawxml.xpath('//cas:berkeleyEduIsMemberOf').map(&:text)
+    affiliations_txt = rawxml.xpath('//cas:berkeleyEduAffiliations').map(&:text)
+
+    { 'berkeleyEduIsMemberOf' => groups_txt, 'berkeleyEduAffiliations' => affiliations_txt }
+  end
+
   provider :cas,
            name: :calnet,
            host: cas_host,
            login_url: '/cas/login',
-           service_validate_url: '/cas/p3/serviceValidate'
+           service_validate_url: '/cas/p3/serviceValidate',
+           fetch_raw_info: fetch_raw_info
 
   # Override the default 'puts' logger that Omniauth uses.
   OmniAuth.config.logger = Rails.logger
